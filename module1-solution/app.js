4f7be50758dbe8986e69c97cde5be9fb03a3901c
stavros <tsourdos@hotmail.com>
2016-11-21 00:26:04 +0200

    initial commit - basic aj template

---
+++ module1-solution/app.js
@@ -0,0 +1,12 @@
+(function () {
+'use strict';
+
+angular.module('LunchApp', [])
+.controller('LunchCheckController', LunchCheckController);
+
+LunchCheckController.$inject = ['$scope'];
+function LunchCheckController($scope) {
+
+}
+
+})();
