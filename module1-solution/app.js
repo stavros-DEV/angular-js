8047f44a830e6fc34a9a6e1a13349acb3e694c5f
stavros <tsourdos@hotmail.com>
2016-11-21 14:40:17 +0200

    lunch check logic added

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -7,6 +7,36 @@ angular.module('LunchApp', [])
 LunchCheckController.$inject = ['$scope'];
 function LunchCheckController($scope) {
 
+  // initializations
+  $scope.dishes = "";
+  $scope.message = "";
+
+  $scope.checkQuantity = function () {
+
+    // split the csv input text, returns an array
+    var dishArray = $scope.dishes.split(',');
+
+    // trim array contents and remove empty strings here
+    dishArray = dishArray.filter(function(e){
+      return e.trim();
+    });
+
+    // main logic here
+    if (dishArray.length == 0) {
+      $scope.message = "Please enter data first";
+      $scope.messageStyle = "message-error";
+      $scope.inputStyle = "input-error";
+    } else if (dishArray.length <= 3) {
+      $scope.message = "Enjoy";
+      $scope.messageStyle = "message-success";
+      $scope.inputStyle = "input-success";
+    } else {
+      $scope.message = "Too much!";
+      $scope.messageStyle = "message-success";
+      $scope.inputStyle = "input-success";
+    }
+  };
+
 }
 
 })();
